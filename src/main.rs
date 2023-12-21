69e65bfd1c527b91959be4c9724ce19931f6796d
Rory Dunne <rorydunne16@yahoo.com>
2023-12-21 18:42:22 -0500

    Finished custom decoder, however the effor to make it work with non-utf chars isn't worth it. Uploading this then switching to serde's library

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,101 @@
+#![allow(unused)]
 use serde_json;
 use std::env;
+use std::path::{Path, PathBuf};
+use std::fs::File;
+use std::io::{self, Read};
 // Available if you need it!
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_value(encoded_value: &str) -> (usize, serde_json::Value) {
-    // If encoded_value starts with a digit, it's a number
-    let next_char = encoded_value.chars().next().unwrap();
-    if next_char.is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[(colon_index + 1)..(colon_index + 1 + number as usize)];
-        // Get this by seeing the size of the prefix number + 1 for the colon, then the length of the string
-        let advance_length = colon_index + number as usize + 1;
-        return (advance_length, serde_json::Value::String(string.to_string()));
+
+fn decode_bencoded_input(encoded_value: &str) -> (serde_json::Value, &str) {
+    match encoded_value.chars().next() {
+        Some('d') => {
+            let mut token_map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+            let mut dict_string = encoded_value.strip_prefix('d').unwrap();
+
+            while !dict_string.is_empty() && !dict_string.starts_with('e') {
+                let (key, val_and_rest) = decode_bencoded_input(dict_string);
+                let (val, rest) = decode_bencoded_input(val_and_rest);
+
+                let key = match key {
+                    serde_json::Value::String(s) => {
+                        s
+                    }
+                    _ => {
+                        panic!("Dictionary Keys Must of Bencode Type String");
+                    }
+                };
+                token_map.insert(key, val);
+                dict_string = rest;
+            }
+            // Need to consume the final 'e' of the dict since it's left out
+            return (token_map.into(), &dict_string[1..]);
+        },
+        Some('l') => {
+            let mut token_list = Vec::new();
+            let mut list_string = encoded_value.strip_prefix('l').unwrap();
+            // Loop through each chunk until you find e
+            while !list_string.is_empty() && !list_string.starts_with('e') {
+                let (val_token, rest_of_str) = decode_bencoded_input(list_string);
+                token_list.push(val_token);
+                list_string = rest_of_str;
+            }
+            // Need to consume the final 'e' of the dict since it's left out
+            return (serde_json::Value::Array(token_list), &list_string[1..]);
+        },
+        Some('i') => {
+            // Split the string at the ending 'e'
+            if let Some((int_string, rest_of_str)) = encoded_value.split_once('e') {
+                // Strip the i out
+                if let Some(trimmed_int_string) = int_string.strip_prefix('i') {
+                    return (serde_json::Value::Number(serde_json::Number::from(i64::from_str_radix(&trimmed_int_string, 10).unwrap())), rest_of_str);
+                }
+            }
+        },
+        // Parse a string value
+        Some('0'..='9') => {
+            // Use if let block to get the string length, and the rest of the encoded string
+            if let Some((str_len, return_string)) = encoded_value.split_once(':') {
+                // Turn the string length into a usize for slicing
+                if let Ok(string_length) = str_len.parse::<usize>() {
+                    // Return a tuple with the actual string sliced out as 1st, and the rest of the string after the slice as second
+                    return (return_string[..string_length].to_string().into(), &return_string[string_length..]);
+                }
+            }
+        },
+        _ => ()
     }
-    else if next_char == 'i' {
-        let integer_length = encoded_value.find('e').unwrap();
-        let int_string = &encoded_value[1..integer_length];
-        return (integer_length+1, serde_json::Value::Number(serde_json::Number::from(i64::from_str_radix(&int_string, 10).unwrap())));
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
+    panic!("Unhandled encoded value: {}", encoded_value)
 }
 
-fn decode_list(encoded_list: &str) -> (usize, Vec<serde_json::Value>) {
-    let mut next_char = encoded_list.chars().peekable();
-    let mut string_iter = encoded_list.chars();
+fn read_torrent_file(path: &Path) -> io::Result<Vec<u8>> {
+    let mut file = File::open(path)?;
 
-    let mut token_list = vec![];
-    let mut advance_length: usize = 0;
-
-    // Move iterators forward to consume the leading 'l'
-    next_char.next();
-    string_iter.next();
-
-    // Decode str
-    while *next_char.peek().unwrap() != 'e' {
-        if next_char.peek().unwrap().is_digit(10) || *next_char.peek().unwrap() == 'i' {
-            let (token_length, val) = decode_value(string_iter.as_str());
-            token_list.push(val);
-            advance_length = token_length;
-        }
-        for _ in 0..advance_length {
-            next_char.next();
-            string_iter.next();
-        }
-    }
-    return (advance_length, token_list);
+    let mut contents = Vec::new();
+    file.read(&mut contents)?;
+    Ok(contents)
 }
 
-fn decode_dict(encoded_list: &str) -> (usize, serde_json::Map<String, serde_json::Value>) {
-    let mut next_char = encoded_list.chars().peekable();
-    let mut string_iter = encoded_list.chars();
-
-    let mut token_map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
-    let mut advance_length: usize = 0;
-
-    // Advance the two iterators to consume the 'd'
-    next_char.next();
-    string_iter.next();
-
-    // Decode str
-    // TODO: Error handle, this will fail ugly mode
-    while *next_char.peek().unwrap() != 'e' {
-        // Read Key
-        let (token_length, key) = decode_value(string_iter.as_str());
-        advance_length = token_length;
-
-        for _ in 0..advance_length {
-            next_char.next();
-            string_iter.next();
-        }
-
-        if next_char.peek().unwrap().is_digit(10) || *next_char.peek().unwrap() == 'i' {
-            let (token_length, val) = decode_value(string_iter.as_str());
-            advance_length = token_length;
-            token_map.insert(String::from(key.as_str().unwrap()), val);
-        }
-        for _ in 0..advance_length {
-            next_char.next();
-            string_iter.next();
-        }
-    }
-    return (advance_length, token_map);
-}
-
-fn decode_bencoded_input(encoded_value: &str) -> serde_json::Value {
-   // If encoded_value starts with a digit, it's a number
-//    let mut chars = encoded_value.chars();
-   let mut peek_chars = encoded_value.chars().peekable();
-   // Get first char
-   let parsed_val: serde_json::Value = match peek_chars.peek().unwrap() {
-       // Parse dict
-       'd' => {
-           let (_, val) = decode_dict(encoded_value);
-           serde_json::Value::Object(val.into())
-       },
-       // Parse list
-       'l' => {
-           let (_, val) = decode_list(encoded_value);
-           serde_json::Value::Array(val)
-       },
-       // Parse String prefixed with number or integer
-       _ => {
-           let (_, val) = decode_value(encoded_value);
-           val
-       }
-   };
-
-   return parsed_val;
-}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_list_decode() {
-        // Remove leading L since the decode_bencoded_input function will strip it out
-        let enc_str = "l5:helloi69ee";
-        let (_, tokens) = decode_list(enc_str);
-
-        println!("{:?}", tokens);
-
-        assert_eq!(tokens, vec![serde_json::json!("hello"), serde_json::json!(69)]);
-    }
-    #[test]
     fn test_map_decode() {
         let enc_str = "d3:foo3:bar5:helloi52ee";
-        let (_, tokens) = decode_dict(enc_str);
+        let (val, rest) = decode_bencoded_input(enc_str);
 
-        assert_eq!(serde_json::Value::Object(tokens.into()), serde_json::json!({"foo": "bar", "hello": 52}));
+        assert_eq!(val, serde_json::json!({"foo": "bar", "hello": 52}));
+    }
+ 
+    #[test]
+    fn test_list_decode() {
+        let enc_str = "l5:helloi69ee";
+        let (tokens, string) = decode_bencoded_input(enc_str);
+        println!("{}",string);
+        assert_eq!(tokens, serde_json::json!(["hello", 69]));
     }
 }
 
@@ -141,11 +104,21 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
+
+
     if command == "decode" {
        // Uncomment this block to pass the first stage
        let encoded_value = &args[2];
-       let decoded_value = decode_bencoded_input(encoded_value);
+       let decoded_value = decode_bencoded_input(encoded_value).0;
        println!("{}", decoded_value.to_string());
+    }
+    else if command == "info" {
+        let torrent_file_path = PathBuf::from(&args[2]);
+        let torrent_data = read_torrent_file(torrent_file_path.as_path());
+
+        //println!("{}", torrent_data.unwrap());
+        println!("{:?}", decode_bencoded_input(torrent_data.unwrap().as_str()).0);
+
     } else {
        println!("unknown command: {}", args[1])
     }
