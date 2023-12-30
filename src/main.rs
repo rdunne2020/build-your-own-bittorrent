9ebac915f11d8c1edcfa5371ae67e08fcdf5153e
Rory Dunne <rorydunne16@yahoo.com>
2023-12-30 10:27:46 -0500

    Get Peers and Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![allow(unused)]
 
-use reqwest::Response;
+mod torrent;
+use torrent::Torrent;
+
+mod tracker;
+use tracker::TrackerResponse;
+
 use serde_json;
 use serde_bencode::de;
 use serde_bytes::ByteBuf;
@@ -9,103 +14,6 @@ use std::env;
 use std::path::{Path, PathBuf};
 use std::fs::File;
 use std::io::{self, Read};
-use sha1::{Digest, Sha1};
-use std::collections::HashMap;
-
-#[allow(dead_code)]
-
-fn urlencode_binary_data(hash: &Vec<u8>) -> String {
-    let mut encoded_string = String::with_capacity(hash.len()*3);
-    for c in hash {
-        match c {
-            b'A'..=b'Z' | b'a'..=b'z' | b'0'..=b'9' | b'-' | b'.' | b'_' | b'~' | b':'| b'?'| b'['| b']'| b'@'| b'!'| b'\''| b'('| b')'| b'*'| b','| b';'| b'='=> {
-                // encoded_string.push_str(&format!("{c:02x}"));
-                encoded_string.push(*c as char);
-            }
-            _ => {
-                encoded_string.push('%');
-                encoded_string.push_str(&format!("{c:02x}"));
-            }
-        }
-    }
-    println!("Encoded String: {}", encoded_string);
-    encoded_string
-}
-
-#[derive(Debug, Deserialize,Serialize)]
-struct TorrentInfo {
-    pub length: i64,
-    pub name: String,
-    // Need this or the parse fails because the key value in the torrent file is "piece length", but serde can't find a matching key value in the struct
-    #[serde(rename="piece length")]
-    pub piece_length: i64,
-    pub pieces: ByteBuf, 
-}
-
-impl TorrentInfo {
-    fn calculate_sha1_hash(&self) -> String {
-        let mut hash = Sha1::new();
-        // hash.update(format!("{:?}", serde_bencode::to_bytes(self).unwrap()));
-        hash.update(serde_bencode::to_bytes(self).unwrap());
-        let result = hash.finalize();
-
-        println!("{:?}", result);
-        // Return hex string
-        return result.iter().map(|byte| format!("{:02x}", byte)).collect::<String>();
-    }
-    fn print_piece_hashes(&self) -> Vec<String>{
-        // Each hash is 20 bytes
-        let hash_string_size = 20;
-        let bytes = self.pieces.to_vec();
-        let num_pieces = bytes.len() / hash_string_size;
-        let mut hash_strings: Vec<String> = Vec::new();
-        for i in 0..num_pieces {
-            let slice_start = i * 20;
-            let slice_end = 20 * (i+1);
-            let byte_slice: &[u8] = &bytes[slice_start..slice_end];
-            hash_strings.push(byte_slice.iter().map(|c| format!("{:02x}", c)).collect::<String>());
-        }
-        hash_strings
-    }
-}
-
-#[derive(Debug, Deserialize)]
-struct Torrent {
-    info: TorrentInfo,
-    #[serde(default)]
-    announce: Option<String>,
-    #[serde(rename="created by")]
-    created_by: Option<String>,
-}
-
-impl Torrent {
-    // Function to request tracker data
-    async fn get_tracker_info(&self) -> Option<Response> {
-        let num_pieces = (self.info.pieces.to_vec().len() / 20);
-        let tracker_url = self.announce.clone().unwrap();
-        // println!("URL: {}", urlencode_binary_data(&serde_bencode::to_bytes(&self.info).unwrap()));
-        // println!("Fart: {:?}", serde_urlencoded::to_string(&self.info));
-        let mut param_map = HashMap::new();
-        param_map.insert("info_hash".to_string(), urlencode_binary_data(&serde_bencode::to_bytes(&self.info).unwrap()));
-        param_map.insert("peer_id".to_string(), "1337694201337694201".to_string());
-        param_map.insert("port".to_string(), "6881".to_string());
-        param_map.insert("uploaded".to_string(), "0".to_string());
-        param_map.insert("downloaded".to_string(), "0".to_string());
-        param_map.insert("length".to_string(), (num_pieces*(self.info.piece_length as usize)).to_string());
-        param_map.insert("compact".to_string(), "1".to_string());
-
-        // Make a request to the URL to get the info about it
-        let tracker_response = reqwest::Client::new()
-                                    .get(tracker_url)
-                                    .query(&param_map)
-                                    .send()
-                                    .await;
-
-        // println!("{:?}", tracker_response);
-        Some(tracker_response.unwrap())
-    }
-}
-
 
 fn decode_bencoded_input(encoded_value: &str) -> (serde_json::Value, &str) {
     match encoded_value.chars().next() {
@@ -222,14 +130,32 @@ async fn main() {
                 }
                 Err(e) => panic!("{}", e)
             };
-        println!("Tracker URL: {}\nLength: {}\nInfo Hash: {}", torrent_data.announce.clone().unwrap(), torrent_data.info.length, torrent_data.info.calculate_sha1_hash());
+        println!("Tracker URL: {}\nLength: {}\nInfo Hash: {}", torrent_data.announce.clone().unwrap(), torrent_data.info.length, torrent_data.info.calculate_sha1_hash().1);
         println!("Piece Length: {}", torrent_data.info.piece_length);
         println!("Piece Hashes:");
         for i in torrent_data.info.print_piece_hashes() {
             println!("{}", i);
         }
-        // TODO: Bad Info Hash
-        println!("{:?}", torrent_data.get_tracker_info().await.unwrap().text().await);
+    }
+    else if command == "peers" {
+       // Uncomment this block to pass the first stage
+        let torrent_file_path = PathBuf::from(&args[2]);
+        let torrent_data = read_torrent_file(torrent_file_path.as_path());
+        let mut torrent_data: Torrent =
+            match torrent_data {
+                Ok(buf) => {
+                    match de::from_bytes::<Torrent>(&buf) {
+                        Ok(t) => t, //torrent_data = t,
+                        Err(e) => panic!("{}", e)
+                    }
+                }
+                Err(e) => panic!("{}", e)
+            };
+        let bin_data = torrent_data.get_tracker_info().await.unwrap().bytes().await.unwrap();
+        let decoded_data: TrackerResponse = serde_bencode::from_bytes(bin_data.as_ref()).unwrap(); 
+        for ip in decoded_data.get_peers() {
+            println!("{}", ip);
+        }
     } else {
        println!("unknown command: {}", args[1])
     }
