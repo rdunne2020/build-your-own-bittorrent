48b41bf17f8c273129d7762b3aeaff4da528f545
Rory Dunne <rorydunne16@yahoo.com>
2024-01-19 13:39:44 -0500

    Added ability to download a piece of a torrent that is specified at the command line. Still need to implement pipelining, error handling, and management of choked/interested state management

---
+++ src/downloads.rs
@@ -0,0 +1,174 @@
+use super::torrent::Torrent;
+
+use super::tracker::HandshakeMessage;
+
+use std::net::{SocketAddrV4, TcpStream};
+use std::io::{Write, Read};
+use std::str::FromStr;
+use std::io::{Error, ErrorKind};
+
+pub struct PeerMessage {
+    pub length: u32,
+    pub message_id: u8,
+    pub payload: Vec<u8>
+}
+
+impl PeerMessage {
+    pub fn new(length: u32, id: u8, data: &Vec<u8>) -> Self {
+        PeerMessage {
+            length,
+            message_id: id,
+            payload: data.clone()
+        }
+    }
+    pub fn get_message_bytes(&self) -> Vec<u8> {
+        let mut byte_vector: Vec<u8> = vec![];
+        let mut length_bytes: [u8; 4] = self.length.to_be_bytes();
+
+        byte_vector.extend_from_slice(&length_bytes);
+        byte_vector.push(self.message_id);
+        byte_vector.append(&mut self.payload.clone());
+
+        byte_vector
+    }
+}
+
+pub enum PeerStatus {
+    PeerChoking,
+    PeerInterested
+}
+
+pub enum DownloaderStatus {
+    DownloaderChoking,
+    DownloaderInterested
+}
+
+pub struct PeerDownloader {
+    tcp_stream: TcpStream,
+    pub peer_status: PeerStatus,
+    pub downloader_status: DownloaderStatus,
+    piece_size: usize,
+    block_size_multiplier: usize
+}
+
+impl PeerDownloader {
+    pub fn new(socket_addr: &str, piece_length: usize) -> Self {
+        let socket_address = SocketAddrV4::from_str(socket_addr).unwrap();
+        println!("{:?}", socket_address);
+        PeerDownloader {
+            tcp_stream: TcpStream::connect(socket_address)
+                .expect(format!("Could Not Initiate Connection to {:?}", socket_addr).as_str()),
+            peer_status: PeerStatus::PeerChoking,
+            downloader_status: DownloaderStatus::DownloaderChoking,
+            piece_size: piece_length,
+            block_size_multiplier: 1024
+        }
+    }
+
+    // TODO take a vector of peer sockets to connect to
+    pub fn handshake(&mut self, torrent_data: &Torrent) {
+        let info_hash: Vec<u8> = torrent_data.info.calculate_sha1_hash().1;
+        let byte_array: [u8; 20] = {
+            let mut arr = [0; 20]; // Initialize an array of zeros
+            arr.copy_from_slice(&info_hash.as_slice()[..20]); // Copy data from slice to array
+            arr
+        };
+        let handshake_msg: HandshakeMessage = HandshakeMessage::new(byte_array);
+        // Create the TCP Stream
+        self.tcp_stream.write_all(&handshake_msg.get_message_bytes());
+
+        let mut response_container: [u8; 68] = [0; 68];
+        self.tcp_stream.read_exact(&mut response_container);
+
+        let peer_id = response_container[48..68].to_owned();
+        println!("Peer ID: {}", hex::encode(peer_id))
+    }
+
+    // TODO: Make this an Option, need better error handling in general
+    pub fn read_peer_message(&mut self) -> PeerMessage{
+        // Read in the first 4 bytes to get message length
+        let mut received_message_length: [u8; 4] = [0; 4];
+        self.tcp_stream.read_exact(&mut received_message_length);
+
+        // Store length in a u32
+        let message_len: u32 = u32::from_be_bytes(received_message_length);
+        let mut received_message_body: Vec<u8> = vec![0; message_len as usize];
+        self.tcp_stream.read_exact(&mut received_message_body);
+
+        // Now Split the first byte of the body out to get message ID, then store the rest as the body
+        let msg_id: u8 = received_message_body[0];
+
+        let payload: Vec<u8> = received_message_body.drain(1..).into_iter().collect();
+
+        // Return the message
+        PeerMessage {
+            length: message_len,
+            message_id: msg_id,
+            payload
+        }
+    }
+
+    pub fn send_peer_message(&mut self, peer_msg: &PeerMessage) -> Result<usize, std::io::Error> {
+        let mut msg_bytes: Vec<u8> = peer_msg.get_message_bytes();
+        let message_length: usize = msg_bytes.len();
+        println!("{:?}", msg_bytes);
+        // Send Message
+        if let Ok(()) = self.tcp_stream.write_all(&mut msg_bytes) {
+            return Ok(message_length);
+        }
+        else {
+            return Err(std::io::Error::new(ErrorKind::WriteZero, "Write to socket failed"));
+        }
+    }
+
+    // TODO: Pipeline this (async)
+    pub fn download_piece(&mut self, piece_index: u32, piece_size: usize, chunk_size: usize) -> Vec<u8> {
+        // Calculate how many chunks we have to download
+        let mut total_chunks: usize = piece_size / chunk_size;
+        // If chunk size doesn't directly divide into piece size, download an extra chunk of a shorter size
+        if piece_size % chunk_size > 0 {
+            total_chunks += 1;
+        }
+
+        let mut piece_data: Vec<u8> = Vec::new();
+        let mut downloaded_bytes: usize = 0;
+        let mut index_byte_array: [u8; 4] = piece_index.to_be_bytes();
+        for i in 0..total_chunks {
+            let mut request_message_byte_array: Vec<u8> = Vec::new();
+            // Make sure you still have enough data in the piece to download a whole chunk
+            if downloaded_bytes + chunk_size < piece_size {
+                let starting_offset: u32 = i as u32 * chunk_size as u32;
+                let mut request_payload: Vec<u8> = Vec::new();
+                let mut begin_byte_array: [u8; 4] = starting_offset.to_be_bytes();
+                let mut length_byte_array: [u8; 4] = (chunk_size as u32).to_be_bytes();
+                request_payload.extend_from_slice(&index_byte_array);
+                request_payload.extend_from_slice(&begin_byte_array);
+                request_payload.extend_from_slice(&length_byte_array);
+                request_message_byte_array= PeerMessage::new(13, 6, &request_payload).get_message_bytes();
+            }
+            // This branch is only hit on the last chunk
+            else {
+                let final_chunk_size: u32 = (chunk_size - (piece_size - downloaded_bytes)) as u32;
+                let starting_offset: u32 = i as u32 * chunk_size as u32;
+                let mut request_payload: Vec<u8> = Vec::new();
+                let mut begin_byte_array: [u8; 4] = starting_offset.to_be_bytes();
+                let mut length_byte_array: [u8; 4] = final_chunk_size.to_be_bytes();
+                request_payload.extend_from_slice(&index_byte_array);
+                request_payload.extend_from_slice(&begin_byte_array);
+                request_payload.extend_from_slice(&length_byte_array);
+                request_message_byte_array= PeerMessage::new(13, 6, &request_payload).get_message_bytes();
+            }
+            if let Ok(()) = self.tcp_stream.write_all(&mut request_message_byte_array) {
+                let mut chunk_data = self.read_peer_message();
+                // Store the payload data into our final vector
+                piece_data.append(&mut chunk_data.payload);
+
+            }
+            // Sending request failed
+            else {
+                return vec![];
+            }
+        }
+        piece_data
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ mod tracker;
 use tracker::TrackerResponse;
 use tracker::HandshakeMessage;
 
+mod downloads;
+use downloads::PeerDownloader;
+use downloads::PeerMessage;
+
 use serde_json;
 use serde_bencode::de;
 use serde_bytes::ByteBuf;
@@ -15,9 +19,16 @@ use std::env;
 use std::path::{Path, PathBuf};
 use std::fs::File;
 use std::io::{self, Read, Write};
-use std::net::SocketAddrV4;
+use std::net::{SocketAddrV4, TcpStream};
 use std::str::FromStr;
-use std::net::TcpStream;
+
+use clap::{arg, Parser, Arg, Command};
+
+fn write_piece_to_file(path: &String, piece_data: &Vec<u8>) -> std::io::Result<u32> {
+    let mut piece_file_io: File = File::create(path)?;
+    piece_file_io.write_all(piece_data)?;
+    Ok(piece_data.len() as u32)
+}
 
 fn decode_bencoded_input(encoded_value: &str) -> (serde_json::Value, &str) {
     match encoded_value.chars().next() {
@@ -99,7 +110,7 @@ mod tests {
 
         assert_eq!(val, serde_json::json!({"foo": "bar", "hello": 52}));
     }
- 
+
     #[test]
     fn test_list_decode() {
         let enc_str = "l5:helloi69ee";
@@ -112,93 +123,163 @@ mod tests {
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
 
-    if command == "decode" {
-       // Uncomment this block to pass the first stage
-       let encoded_value = &args[2];
-       let decoded_value = decode_bencoded_input(encoded_value).0;
-       println!("{}", decoded_value.to_string());
-    }
-    else if command == "info" {
-        let torrent_file_path = PathBuf::from(&args[2]);
-        let torrent_data = read_torrent_file(torrent_file_path.as_path());
-        let mut torrent_data: Torrent =
-            match torrent_data {
-                Ok(buf) => {
-                    match de::from_bytes::<Torrent>(&buf) {
-                        Ok(t) => t, //torrent_data = t,
-                        Err(e) => panic!("{}", e)
-                    }
-                }
-                Err(e) => panic!("{}", e)
-            };
-        println!("Tracker URL: {}\nLength: {}\nInfo Hash: {}", torrent_data.announce.clone().unwrap(), torrent_data.info.length, torrent_data.info.print_info_hash_hex());
-        println!("Piece Length: {}", torrent_data.info.piece_length);
-        println!("Piece Hashes:");
-        for i in torrent_data.info.print_piece_hashes() {
-            println!("{}", i);
-        }
-    }
-    else if command == "peers" {
-       // Uncomment this block to pass the first stage
-        let torrent_file_path = PathBuf::from(&args[2]);
-        let torrent_data = read_torrent_file(torrent_file_path.as_path());
-        let mut torrent_data: Torrent =
-            match torrent_data {
-                Ok(buf) => {
-                    match de::from_bytes::<Torrent>(&buf) {
-                        Ok(t) => t, //torrent_data = t,
-                        Err(e) => panic!("{}", e)
-                    }
-                }
-                Err(e) => panic!("{}", e)
-            };
-        let bin_data = torrent_data.get_tracker_info().await.unwrap().bytes().await.unwrap();
-        let decoded_data: TrackerResponse = serde_bencode::from_bytes(bin_data.as_ref()).unwrap(); 
-        for ip in decoded_data.get_peers() {
-            println!("{}", ip);
-        }
-    }
-    else if command == "handshake" {
-        let torrent_file_path = PathBuf::from(&args[2]);
-        let torrent_data = read_torrent_file(torrent_file_path.as_path());
-        let mut torrent_data: Torrent =
-            match torrent_data {
-                Ok(buf) => {
-                    match serde_bencode::de::from_bytes::<Torrent>(&buf) {
-                        Ok(t) => t, //torrent_data = t,
-                        Err(e) => panic!("{}", e)
-                    }
-                }
-                Err(e) => panic!("{}", e)
-            };
-        let socket_str = &args[3];
-        let socket_obj = SocketAddrV4::from_str(&socket_str.as_str());
-        match socket_obj {
-            Ok(sock) => {
-                let info_hash = torrent_data.info.calculate_sha1_hash().1;
-                let byte_array: [u8; 20] = {
-                    let mut arr = [0; 20]; // Initialize an array of zeros
-                    arr.copy_from_slice(&info_hash.as_slice()[..20]); // Copy data from slice to array
-                    arr
+    let cmd = Command::new("rTorrent")
+        .bin_name("rTorrent")
+        .subcommand_required(true)
+        .subcommand(Command::new("download_piece")
+            .about("Download one specific piece of a file")
+            .args(&[
+                //arg!(-o <OUTPUT> "File to save the piece to"),
+                Arg::new("output_file")
+                .required(true)
+                .short('o')
+                .long("output")
+                .help("File to save the piece to"),
+                arg!(<torrentfile> "Torrent File to Read"),
+                arg!(<piecenum> "Which Piece Number to Download")
+            ])
+    );
+
+    let matches = cmd.get_matches();
+
+    match matches.subcommand_name() {
+        // If the command is download_piece
+        Some("download_piece") => {
+            let dl = matches.subcommand_matches("download_piece").unwrap();
+            let output: &String = dl.get_one::<String>("output_file").expect("Need Output File passed in");
+            let torrentfile: &String = dl.get_one::<String>("torrentfile").expect("Need torrent file passed in");
+            let piecenum: u32 = dl.get_one::<String>("piecenum")
+                .expect("Need piece number passed in")
+                .parse::<u32>().unwrap();
+
+            if let Ok(torrent_bytes) = read_torrent_file(PathBuf::from(torrentfile.as_str()).as_path()) {
+                let mut torrent_data: Torrent = de::from_bytes::<Torrent>(&torrent_bytes).unwrap();
+                let binary_tracker_info = torrent_data.get_tracker_info().await.unwrap().bytes().await.unwrap();
+                let decoded_tracker_data: TrackerResponse =
+                    serde_bencode::from_bytes(&binary_tracker_info.as_ref()).unwrap();
+                let trackers: &Vec<String> = &decoded_tracker_data.get_peers();
+
+                // Setup the DL helper, then handshake and start passing messages
+                let mut dl_helper: PeerDownloader = PeerDownloader::new(trackers[1].as_str(), torrent_data.info.piece_length as usize);
+                dl_helper.handshake(&torrent_data);
+                let bitfield_msg = dl_helper.read_peer_message();
+
+                let interested_message: PeerMessage = PeerMessage {
+                    length: 1,
+                    message_id: 2,
+                    payload: vec![]
                 };
-                let handshake_msg: HandshakeMessage = HandshakeMessage::new(byte_array);
-                // Create the TCP Stream
-                let mut stream = TcpStream::connect(sock).expect(format!("Could Not Initiate Connection to {:?}", sock).as_str());
-                stream.write_all(&handshake_msg.get_message_bytes());
+                // Unchoke downloader
+                dl_helper.downloader_status = downloads::DownloaderStatus::DownloaderInterested;
+                // Send Interested Message
+                dl_helper.send_peer_message(&interested_message);
 
-                let mut response_container: [u8; 68] = [0; 68];
-                stream.read_exact(&mut response_container);
+                let unchoke_message = dl_helper.read_peer_message();
+                // Once the unchoke message is received we can start requesting data
+                dl_helper.peer_status = downloads::PeerStatus::PeerInterested;
+                println!("{}", unchoke_message.message_id);
+                // Download 16k chunks (16384 bytes)
+                let piece = dl_helper.download_piece(piecenum, torrent_data.info.piece_length as usize, 16384);
+                println!("Downloaded piece of length: {}", piece.len());
 
-                let peer_id = response_container[48..68].to_owned();
-                println!("Peer ID: {}", hex::encode(peer_id))
-            },
-            Err(e) => println!("{:?}", e) 
-        };
-    } 
-    else {
-       println!("unknown command: {}", args[1])
+                println!("Wrote piece {} to file: {} (Size {})",
+                    piecenum, output, write_piece_to_file(output, &piece).unwrap());
+            }
+            else {
+                println!("Failed to Read Torrent File");
+            }
+        }
+        _ => ()
     }
+
+    // let args: Vec<String> = env::args().collect();
+    // let command = &args[1];
+
+    // if command == "decode" {
+    //    // Uncomment this block to pass the first stage
+    //    let encoded_value = &args[2];
+    //    let decoded_value = decode_bencoded_input(encoded_value).0;
+    //    println!("{}", decoded_value.to_string());
+    // }
+    // else if command == "info" {
+    //     let torrent_file_path = PathBuf::from(&args[2]);
+    //     let torrent_data = read_torrent_file(torrent_file_path.as_path());
+    //     let mut torrent_data: Torrent =
+    //         match torrent_data {
+    //             Ok(buf) => {
+    //                 match de::from_bytes::<Torrent>(&buf) {
+    //                     Ok(t) => t, //torrent_data = t,
+    //                     Err(e) => panic!("{}", e)
+    //                 }
+    //             }
+    //             Err(e) => panic!("{}", e)
+    //         };
+    //     println!("Tracker URL: {}\nLength: {}\nInfo Hash: {}", torrent_data.announce.clone().unwrap(), torrent_data.info.length, torrent_data.info.print_info_hash_hex());
+    //     println!("Piece Length: {}", torrent_data.info.piece_length);
+    //     println!("Piece Hashes:");
+    //     for i in torrent_data.info.print_piece_hashes() {
+    //         println!("{}", i);
+    //     }
+    // }
+    // else if command == "peers" {
+    //    // Uncomment this block to pass the first stage
+    //     let torrent_file_path = PathBuf::from(&args[2]);
+    //     let torrent_data = read_torrent_file(torrent_file_path.as_path());
+    //     let mut torrent_data: Torrent =
+    //         match torrent_data {
+    //             Ok(buf) => {
+    //                 match de::from_bytes::<Torrent>(&buf) {
+    //                     Ok(t) => t, //torrent_data = t,
+    //                     Err(e) => panic!("{}", e)
+    //                 }
+    //             }
+    //             Err(e) => panic!("{}", e)
+    //         };
+    //     let bin_data = torrent_data.get_tracker_info().await.unwrap().bytes().await.unwrap();
+    //     let decoded_data: TrackerResponse = serde_bencode::from_bytes(bin_data.as_ref()).unwrap();
+    //     for ip in decoded_data.get_peers() {
+    //         println!("{}", ip);
+    //     }
+    // }
+    // else if command == "handshake" {
+    //     let torrent_file_path = PathBuf::from(&args[2]);
+    //     let torrent_data = read_torrent_file(torrent_file_path.as_path());
+    //     let mut torrent_data: Torrent =
+    //         match torrent_data {
+    //             Ok(buf) => {
+    //                 match serde_bencode::de::from_bytes::<Torrent>(&buf) {
+    //                     Ok(t) => t, //torrent_data = t,
+    //                     Err(e) => panic!("{}", e)
+    //                 }
+    //             }
+    //             Err(e) => panic!("{}", e)
+    //         };
+    //     let socket_str = &args[3];
+    //     let socket_obj = SocketAddrV4::from_str(&socket_str.as_str());
+    //     match socket_obj {
+    //         Ok(sock) => {
+    //             let info_hash = torrent_data.info.calculate_sha1_hash().1;
+    //             let byte_array: [u8; 20] = {
+    //                 let mut arr = [0; 20]; // Initialize an array of zeros
+    //                 arr.copy_from_slice(&info_hash.as_slice()[..20]); // Copy data from slice to array
+    //                 arr
+    //             };
+    //             let handshake_msg: HandshakeMessage = HandshakeMessage::new(byte_array);
+    //             // Create the TCP Stream
+    //             let mut stream = TcpStream::connect(sock).expect(format!("Could Not Initiate Connection to {:?}", sock).as_str());
+    //             stream.write_all(&handshake_msg.get_message_bytes());
+
+    //             let mut response_container: [u8; 68] = [0; 68];
+    //             stream.read_exact(&mut response_container);
+
+    //             let peer_id = response_container[48..68].to_owned();
+    //             println!("Peer ID: {}", hex::encode(peer_id))
+    //         },
+    //         Err(e) => println!("{:?}", e)
+    //     };
+    // }
+    // else {
+    //    println!("unknown command: {}", args[1])
+    // }
 }
